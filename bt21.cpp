de2adfc9785e072deb8e64201229da2fd398fe4a
Tran An Tuong <cautuong07@gmail.com>
2026-06-21 22:02:07 +0700

    Luu lan 2

--- bt21.cpp
+++ bt21.cpp
@@ -7,15 +7,15 @@ int main() {
 
     if (a + b > c && a + c > b && b + c > a) {
         if (a == b && b == c) {
-            printf("Tam giac deu");
-        } else if (a == b || a == c || b == c) {
+            printf("Tam giac deu co ba canh bang nhau");
+        } else if (a == b || a == c || b == c) {    
             if (a * a + b * b == c * c || a * a + c * c == b * b || b * b + c * c == a * a) {
                 printf("Tam giac vuong can");
             } else {
                 printf("Tam giac can");
             }
         } else if (a * a + b * b == c * c || a * a + c * c == b * b || b * b + c * c == a * a) {
-            printf("Tam giac vuong");
+            printf("Tam giac vuong co 1 goc 90 do");
         } else {
             printf("Tam giac thuong");
         }
